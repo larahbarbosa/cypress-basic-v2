4cac3e0da9226c1b09aaf45fd5aa4cdd05ef5347
Larissa Barbosa <larissabarbosa@id.uff.br>
2023-05-06 12:32:42 -0300

    success message

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,7 +1,20 @@
 
 describe('Central de Atendimento ao Cliente TAT', function() {
+
+    beforeEach(() => {
+        cy.visit('./src/index.html')  
+    })
+
     it('verifica o título da aplicação', function() {
-        cy.visit('./src/index.html')
         cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
+    })
+
+    it('Preenche os campos obrigatórios e envia o formulário', function() {
+        cy.get('#firstName').type('Larissa')
+        cy.get('#lastName').type('Barbosa')
+        cy.get('#email').type('larissa@teste.com')
+        cy.get('#open-text-area').type('Teste')
+        cy.get('button[type="submit"]').click()
+        cy.get('.success').should('be.visible')
     })
   })
