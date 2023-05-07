624545293dab1b38ff7f560169853118c678ea0d
Larissa Barbosa <larissabarbosa@id.uff.br>
2023-05-07 16:35:59 -0300

    delay property

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -10,10 +10,11 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
 
     it('Preenche os campos obrigatórios e envia o formulário', function() {
+        const longText = 'Test long test delay Test long test delayTest long test delayTest long test delayTest long test delay'
         cy.get('#firstName').type('Larissa')
         cy.get('#lastName').type('Barbosa')
         cy.get('#email').type('larissa@teste.com')
-        cy.get('#open-text-area').type('Teste')
+        cy.get('#open-text-area').type(longText, { delay:0 })
         cy.get('button[type="submit"]').click()
         cy.get('.success').should('be.visible')
     })
