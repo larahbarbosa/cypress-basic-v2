aa944cf0bbb581071c6cdc02c77fcca6f4f6afc4
Larissa Barbosa <larissa.barbosa@sanar.com>
2023-05-31 21:49:55 -0300

    invoke & request

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -189,5 +189,45 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             .click()
     })
 
+    it('Exibe e esconde as mensagens de sucesso e erro usando o .invoke', () => {
+        cy.get('.success')
+          .should('not.be.visible')
+          .invoke('show')
+          .should('be.visible')
+          .and('contain', 'Mensagem enviada com sucesso.')
+          .invoke('hide')
+          .should('not.be.visible')
+        cy.get('.error')
+          .should('not.be.visible')
+          .invoke('show')
+          .should('be.visible')
+          .and('contain', 'Valide os campos obrigatórios!')
+          .invoke('hide')
+          .should('not.be.visible')
+      })
 
-  })
+    it('Preenche a area de texto usando o comando invoke', () => {
+        const longText = Cypress._.repeat('0123456789', 20)
+        
+        cy.get('#open-text-area')
+            .invoke('val', longText)
+            .should('have.value', longText)
+            
+    })
+
+    it('Faz uma requisição HTTP', () => {
+        cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+            .should(function(response) {
+                const { status, statusText, body } = response
+                expect(status).to.equal(200)
+                expect(statusText).to.equal('OK')
+                expect(body).to.include('CAC TAT')
+
+            })
+        })
+
+
+    
+})
+
+
